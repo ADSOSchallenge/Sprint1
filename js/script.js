e53ab4dfd06545dc9ffe004bb8835c2b4eee5c07
Etvtaeyo <ericknakajune10@gmail.com>
2025-05-14 17:46:18 -0300

    Arcordeão para as perguntas do FAQ

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//Acordeão do faq
+const accordions = document.querySelectorAll('.accordion');
+
+accordions.forEach((accordion) => {
+    accordion.addEventListener('click', function () {
+        this.classList.toggle('active');
+        const panel = this.nextElementSibling;
+        panel.classList.toggle('show');
+    });
+});
